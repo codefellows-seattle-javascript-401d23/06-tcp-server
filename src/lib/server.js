b5ed81e1fa3980cc1829df20c18de29e64930f56
sarasaurus <bixler.s.e@gmail.com>
2018-04-24 08:51:56 -0700

    trouble shooting basic server funcitonality---working on how to generate users from actual entered data

--- src/lib/server.js
+++ src/lib/server.js
@@ -5,24 +5,70 @@ const logger = require('./logger');
 
 // ---------------------------------
 const app = net.createServer();
-let clients = [];
+const clients = [];
 // ---------------------------------
+
+const parseCommand = (message, socket) => {
+  if (!message.startsWith('@')) {
+    return false;
+  }
+  const parsedMesage = message.split(' ');
+  const command = parsedMessage[0];
+  logger.log(logger.INFO, `parsing a command ${command}`);
+
+  // switch
+  switch (command) {
+    case '@list': {
+      const clientNames = clients.map(client => client.name).join('\n');
+      break;
+    }
+    default: {
+      socket.write('INVALID COMMAND!');
+      break;
+    }
+    // return true;
+  }
+
+  const removeClient = socket => () => {
+  // clients = clients.filter(client => client !== socket);
+    logger.log(logger.INFO, `Removing ${socket.name}`);
+  };
+};// parsecommand
 
 app.on('connection', (socket) => {
   logger.log(logger.INFO, 'new socket');
-  //clients.push(socket);
-  socket.write('welcome to the chat!\n');
-  // socket.name =
-  //socket.write(`Your name is ${socket.name}\n`);
-//----------------------------------
-//events
-//-------------------
-// socket.on('data', (data) => {
-//   const message = data.toString().trim();
-// logger.log(logger.INFO, `processing a message: ${message});
-// })
+  clients.push(socket);
+  socket.write('welcome to the chat! enter a name for yourself!');
 
-});
+  socket.name = 'poop';
+  socket.write(`Your name is ${socket.name}\n`);
+  // ----------------------------------
+  // socket events
+  // -------------------
+  socket.on('data', (data) => {
+    const message = data.toString().trim();
+
+    // data checks 
+
+    logger.log(logger.INFO, `processing a message: ${message}`);
+
+    // command check:
+
+    if (parseCommand(message, socket)) {
+      return;
+    }// if
+    clients.forEach((client) => {
+      if (client !== socket) {
+        client.write(`${socket.name}: ${message}\n`);
+      }// if
+    });// for each
+  });// closig out socket.on
+  socket.on('close', removeClient(socket));
+  socket.on('error', () => {
+    logger.log(logger.ERROR, socket.name);
+    removeClient(socket)();
+  });// on error
+});// closing out big app.on
 
 const server = module.exports = {};
 
@@ -37,4 +83,4 @@ server.start = () => {
 server.stop = () => {
   logger.log(logger.INFO, 'Server is offline');
   return app.close(() => {});
-};
+};  
